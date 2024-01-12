de1bf7cacfc66fa551566bfebc9cbd71a218461b
erin-major <erinmajor254@gmail.com>
2024-01-11 19:20:01 -0700

    Added code to create a number list and give the sum, average, and largest number.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+            
+        } while (userNumber != 0);
+
+        double total = 0;
+        int max = -1;
+
+        foreach (int number in numbers)
+        {
+            total += number;
+
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        double average = total / numbers.Count;
+
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+
+
     }
 }
