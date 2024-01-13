7379ae8aa358764f1c69918a66305f0da7086f01
erin-major <erinmajor254@gmail.com>
2024-01-12 17:50:20 -0700

    Add program to intake name and favorite number and square the number

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+
+        DisplayResult(square, name); 
+
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favNum = int.Parse(Console.ReadLine());
+
+        return favNum; 
+    }
+
+    static int SquareNumber(int number)
+    {
+        int squaredNum = number * number;
+
+        return squaredNum;
+    }
+
+    static void DisplayResult(int number, string name)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
     }
 }
