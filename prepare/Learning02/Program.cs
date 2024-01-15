df57c6b4a07a50156b60120842b52308a343e1e6
erin-major <erinmajor254@gmail.com>
2024-01-15 11:55:20 -0700

    Created classes for jobs and resumes and called them in the main class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        job1._company = "Filevine";
+        job1._jobTitle = "QA Analyst 1";
+        job1._startYear = 2021;
+        job1._endYear = 2024;
+
+        // job1.DisplayJobDetails();
+
+        job2._company = "Amazon";
+        job2._jobTitle = "Associate";
+        job2._startYear = 2020;
+        job2._endYear = 2021;
+
+        // job2.DisplayJobDetails();
+
+        Resume resume = new Resume();
+    
+        resume._name = "Erin Major";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayResumeDetails();
+        
+
     }
 }
