109d6bf5eccc6d6aac91e2ec698093254e87be91
erin-major <erinmajor254@gmail.com>
2024-01-29 20:13:58 -0700

    Added Assignment class and sub classes for Math and Writing Assignments

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -3,7 +3,18 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning04 World!");
+    {        
+        Assignment assignment1 = new Assignment("Erin Major", "Coding");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathAssignment1 = new MathAssignment("Benjamin Hugh", "Algebra", "12.5", "10-30");
+        Console.WriteLine();
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Mary Burr", "Religion", "Tips and Tricks for Daily Study");
+        Console.WriteLine();
+        Console.WriteLine(writingAssignment1.GetSummary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
     }
 }
