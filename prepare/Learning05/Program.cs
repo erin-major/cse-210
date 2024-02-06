66d210b5adcda3bc33824be1d4938eb1ee1dbb2c
erin-major <erinmajor254@gmail.com>
2024-02-05 17:43:23 -0700

    Added the shape, square, rectangle, and circle classes. Called methods in the classes in the main program.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square sq = new Square("Green", 5);
+        Console.WriteLine(sq.GetColor());
+        Console.WriteLine(sq.GetArea());
+
+        Console.WriteLine();
+
+        Rectangle rec = new Rectangle("Purple", 5, 7);
+        Console.WriteLine(rec.GetColor());
+        Console.WriteLine(rec.GetArea());
+
+        Console.WriteLine();
+
+        Circle cir = new Circle("Pink", 5);
+        Console.WriteLine(cir.GetColor());
+        Console.WriteLine(cir.GetArea());
+
+        Console.WriteLine("\nList:");
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(sq);
+        shapes.Add(rec);
+        shapes.Add(cir);
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+            Console.WriteLine();
+        }
+
+
     }
 }
