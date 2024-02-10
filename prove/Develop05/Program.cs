b8f2857112ac670ad749a7960b5e3ff1d5a0245f
erin-major <erinmajor254@gmail.com>
2024-02-10 11:17:53 -0700

    Added last comment for extra work

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -12,3 +12,5 @@ class Program
 
 // I exceeded expectations by adding a try catch block when loading the file.
 // I added a catch if the user input something other than 1-6 for the menu.
+// Lastly, I added handling if the user tries to list goals or record an event and they don't
+// have goals. 
